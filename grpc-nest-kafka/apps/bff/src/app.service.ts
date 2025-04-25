b44ea548ae9acf394dc5f16b121e3f295e52f2b1
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-04-25 16:53:11 +0100

    fix: cleaned up config so that building protos works, and it can be imported into NestJS apps

--- grpc-nest-kafka/apps/bff/src/app.service.ts
+++ grpc-nest-kafka/apps/bff/src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { FooServiceClient } from '@repo/protos';
 
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log(FooServiceClient)
     return 'Hello World!';
   }
 }
