6ba453356cbe536eeb3fad9457c606d8f9a165fa
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-04-26 15:17:47 +0100

    feat: converted nestjs apps to microservices and hooked up to protos

--- grpc-nest-kafka/apps/date/src/main.ts
+++ grpc-nest-kafka/apps/date/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'date',             
+        protoPath: require.resolve('@repo/protos/protos/date.proto'),
+        url: process.env.DATE_GRPC_URL || '0.0.0.0:50051',
+      },
+    },
+  );
+  await app.listen();
+  console.log('Date gRPC server listening');
 }
 bootstrap();
--- grpc-nest-kafka/apps/weather/src/main.ts
+++ grpc-nest-kafka/apps/weather/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3003);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'weather',
+        protoPath: require.resolve('@repo/protos/protos/weather.proto'),
+        url: process.env.QUOTE_OF_THE_DAY_GRPC_URL || '0.0.0.0:50053',
+      },
+    },
+  );
+  await app.listen();
+  console.log('Weather gRPC server listening');
 }
 bootstrap();
