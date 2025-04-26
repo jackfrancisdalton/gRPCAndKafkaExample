8d9ec5b888c0f48e3d71b3c678ad3be186c5808a
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-04-26 15:07:25 +0100

    feat: added proto types

--- grpc-nest-kafka/packages/protos/src/index.ts
+++ grpc-nest-kafka/packages/protos/src/index.ts
@@ -1,1 +1,4 @@
-export * from './generated/foo';
+export * as date    from './generated/date';
+export * as weather from './generated/weather';
+export * as quote   from './generated/quote-of-the-day';
+export * as bff     from './generated/bff';
